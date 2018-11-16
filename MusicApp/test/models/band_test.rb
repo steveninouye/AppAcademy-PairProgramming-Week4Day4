45057fe9bd2253ddb29a99ae6b1909bb54835af1
Steven Inouye <steveninouye@msn.com>
2018-11-15 17:12:18 -0800

    Add Album and Band class

--- MusicApp/test/models/band_test.rb
+++ MusicApp/test/models/band_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: bands
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class BandTest < ActiveSupport::TestCase
