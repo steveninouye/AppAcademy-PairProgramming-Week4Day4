bbc46efec3980b2ae1671534ab97b704199fd225
Steven Inouye <steveninouye@msn.com>
2018-11-15 17:02:09 -0800

    Remove active storage routes

--- MusicApp/config/routes.rb
+++ MusicApp/config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :tracks
   resources :bands, only: [:show] do
     resources :albums, only: [:new]
   end
-  resources :users #, only: [:create, :new, :show]
+  resources :albums, only: [:show] do
+    resources :tracks, only: [:new]
+  end
   resource :session, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :band
-  resources :albums
+  resources :tracks, only: [:create, :edit, :show, :update, :destroy]
+  resources :albums, only: [:create, :edit, :show, :update, :destroy]
+  resources :bands
+  resources :users #, only: [:create, :new, :show]
 end
